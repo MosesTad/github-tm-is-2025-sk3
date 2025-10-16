9ed291cffc4714c7dc8d3da8738e0eca906217e5
TadeasMojzis <tadeas.mojzis@tul.cz>
2025-10-16 16:30:19 +0200

    #4 součet cifer

--- IS-Programy/program001-vypis-rady/Program.cs
+++ IS-Programy/program001-vypis-rady/Program.cs
@@ -19,8 +19,23 @@ while (again == "a")
     int first;
     while (!int.TryParse(Console.ReadLine(), out first))
     {
-        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znavu: ");
+        Console.Write("Nezadali jste celé číslo. Zadejte první číslo řady znovu: ");
 }
+
+    Console.Write("Zadejte poslední číslo řady(celé číslo): ");
+    int last;
+    while (!int.TryParse(Console.ReadLine(), out last))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte poslední číslo řady znovu: ");
+}
+
+    Console.Write("Zadejte diferenci (celé číslo): ");
+    int step;
+    while (!int.TryParse(Console.ReadLine(), out step))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte diferenci znovu: ");
+}
+
 
 
 
