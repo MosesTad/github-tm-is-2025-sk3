5cd12cbc1b3b0769ecc3f1a7deec4e6de0f3879e
TadeasMojzis <tadeas.mojzis@tul.cz>
2025-10-23 12:48:51 +0200

    #4 součet cifer DÚ

---
+++ IS-Programy/program002b-součet-cifer/program002b-součet-cifer/Program.cs
@@ -0,0 +1,45 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("********************************************");
+    Console.WriteLine("********** Součet a součin cifer ***********");
+    Console.WriteLine("********************************************");
+    Console.WriteLine("************** Tadeáš Mojžíš ***************");
+    Console.WriteLine("*************** 22.10.2025 *****************");
+    Console.WriteLine("********************************************");
+    Console.WriteLine();
+
+    // Vstup čísla
+    Console.Write("Zadejte celé číslo: ");
+    string input = Console.ReadLine();
+
+    // Kontrola, zda uživatel zadal číslo
+    while (!int.TryParse(input, out _))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte ho znovu: ");
+        input = Console.ReadLine();
+    }
+
+    string numberStr = input;
+
+    // Pokud je číslo záporné, odstraníme "-"
+    if (numberStr.StartsWith("-"))
+    {
+        numberStr = numberStr.Substring(1);
+    }
+
+    int suma = 0;
+
+    // Procházíme každý znak a převádíme na číslo
+    foreach (char c in numberStr)
+    {
+        suma += c - '0';
+    }
+
+    Console.WriteLine($"Součet cifer čísla {input} je {suma}");
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
